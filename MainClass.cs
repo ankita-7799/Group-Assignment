9d5acc33c1a0b299466eee37d46cc27092d63cdf
Ankita <Ankita@LAPTOP-80FTJLO3>
2024-01-01 19:59:39 +0530

    Add project files.

---
+++ MainClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Assignment
+{
+     class Program
+    {
+         static void Main(string[] args)
+        {
+            //    Owner owner = new Owner( 123,"Ankita", 25000 );
+            //    owner.ShowOwnerDetails();
+            //    Console.ReadLine();
+
+            string name;
+            string reverse = "";
+            Console.WriteLine("Enter String To Be Reverse");
+            name = Console.ReadLine();
+
+            Console.WriteLine("string is : {0}", name);
+            int length = name.Length -1;
+
+            while (length >= 0)
+            { 
+                reverse = reverse + name[length];
+                length--;
+            }
+            Console.WriteLine("reverse : {0}", reverse);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Assignment
+{
+    class Owner
+    {
+        public int OwnerID;
+        public string OwnerName;
+        public double TotalProfit;
+
+        double TaxDeduction = 0.1;
+        double NetProfit;
+
+        public Owner(int ownerID, string ownerName, double totalProfit)
+        {
+            this.OwnerID = ownerID;
+            this.OwnerName = ownerName;
+            this.TotalProfit = totalProfit;
+
+        }
+
+        void CalculateProfit()
+        {
+          if (TotalProfit >= 30000)
+            {
+               NetProfit = TotalProfit - (TaxDeduction * TotalProfit);
+                Console.WriteLine("Your total profit is: {0}", NetProfit);
+            }
+           else
+            {
+                Console.WriteLine("Your total profit is: {0}", TotalProfit);
+            }
+        
+        }  
+        public void ShowOwnerDetails()
+        {
+            Console.WriteLine("OwnerID is : {0}", this.OwnerID);
+            Console.WriteLine("OwnerName is : {0}", this.OwnerName);
+            this.CalculateProfit();
+       
+        }
+
+
+    }
+}   
